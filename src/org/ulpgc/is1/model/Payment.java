520755a137ec284e402fb31963feaaa487574552
Mcgregor00 <oussamaiestablero1@gmail.com>
2024-10-18 10:10:42 +0100

    Clase Payment finalizada

--- src/org/ulpgc/is1/model/Payment.java
+++ src/org/ulpgc/is1/model/Payment.java
@@ -1,4 +1,39 @@
 package org.ulpgc.is1.model;
 
+import java.time.LocalDate;
+
 public class Payment {
+    private LocalDate date;
+    private int amount;
+    private String card;
+
+    public Payment(int amount, String card) {
+        this.amount = amount;
+        this.card = card;
+        this.date = LocalDate.now();
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getCard() {
+        return card;
+    }
+
+    public void setCard(String card) {
+        this.card = card;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
 }
