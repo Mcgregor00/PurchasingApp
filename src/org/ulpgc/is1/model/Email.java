b62d5d031abc6cb307ebb0febcbe1069b7c31ea7
Mcgregor00 <oussamaiestablero1@gmail.com>
2024-10-18 09:50:14 +0100

    Clase Email finalizada

--- src/org/ulpgc/is1/model/Email.java
+++ src/org/ulpgc/is1/model/Email.java
@@ -1,5 +1,27 @@
 package org.ulpgc.is1.model;
 
 public class Email {
+    private String account;
+    private final String[] dominiosValidos;
 
+    public Email(String account) {
+        this.account = account;
+        this.dominiosValidos = new String[]{"@gmail.com", "@hotmail.com", "@outlook.com"};
+    }
+    public boolean isValid() {
+        for (String dominio : dominiosValidos) {
+            if (account.endsWith(dominio)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
 }
